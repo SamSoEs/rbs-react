f150ed5193d7857f09002440e5ab63dd76b3cd40
Saman <samaneghbali1@gmail.com>
2022-12-27 10:08:49 -0500

    Add Rentals to DB

--- server/controllers/rentals.js
+++ server/controllers/rentals.js
@@ -21,7 +21,24 @@
   }
   
   exports.createRental = (req, res) => {
-   
+    const rentalData = req.body;
+  // const newRental = new Rental(rentalData);
+
+  // newRental.save((error, createdRental) => {
+    // if (error) {
+    //   return res.status(422).send({errors: [{title: 'Rental Error!', message: 'Cannot post rental data!'}]})
+    // }
+
+    // return res.json({message: `Rental with id: ${createdRental._id} was added!`});
+  // })
+
+  Rental.create(rentalData, (error, createdRental) => {
+    if (error) {
+      return res.status(422).send({errors: [{title: 'Rental Error!', message: error.message}]})
+    }
+
+    return res.json({message: `Rental with id: ${createdRental._id} was added!`});
+  })
   }
   
  
