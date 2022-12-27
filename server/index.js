75ce260d9e9f54e108cefdf80c4f2cff5b536035
Saman <samaneghbali1@gmail.com>
2022-12-27 07:36:59 -0500

    DB Setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const rentalRoutes = require('./routes/rentals');
+
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+mongoose.connect('mongodb+srv://samaneghbali1:Sa12344321@cluster0.kx9khxy.mongodb.net/rbs-react?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => {
+  console.log('Connected to DB!')
+});
 
 app.use(bodyParser.json())
 app.use('/api/v1/rentals', rentalRoutes);
