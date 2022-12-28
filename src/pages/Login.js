c9c2b5b82b5e2f6886def86ba6493cc01813d330
Saman <samaneghbali1@gmail.com>
2022-12-28 16:12:38 -0500

    Get Values from Login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,21 @@
 import React from 'react';
 class Login extends React.Component {
 
+  state = {
+    email: 'test@gmail.com',
+    password: ''
+  }
+
+  handleInputChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = () => {
+    alert(JSON.stringify(this.state));
+  }
+
+
   render() {
 
     return (
@@ -8,41 +23,32 @@ class Login extends React.Component {
         <div className="row">
           <div className="col-md-5">
             <h1 className="page-title">Login</h1>
-            {/* <!-- <div className="alert alert-success">
-              Some message
-            </div> --> */}
             <form>
               <div className="form-group">
                 <label htmlFor="email">Email</label>
-                <input 
+                <input
+                  onChange={this.handleInputChange}
+                  name="email"
                   type="email"
                   className="form-control"
+                  value={this.state.email}
                   id="email" />
-                {/* <div className="alert alert-danger">
-                  <div>
-                    Email is required.
-                  </div>
-                  <div>
-                    Must be valid email format!
-                  </div>
-                </div> */}
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input 
+                <input
+                  onChange={this.handleInputChange}
+                  name="password"
+                  value={this.state.password}
                   type="password"
                   className="form-control"
                   id="password" />
               </div>
-              <button 
-                type="submit" 
+              <button
+                onClick={this.handleSubmit}
+                type="button"
                 className="btn btn-bwm-main">Submit</button>
             </form>
-            {/* <div className="alert alert-danger">
-              <p>
-                Some Error
-              </p>
-            </div> --> */}
           </div>
           <div className="col-md-6 ml-auto">
             <div className="image-container">
@@ -51,7 +57,7 @@ class Login extends React.Component {
             </div>
           </div>
         </div>
-      </div> 
+      </div>
     )
   }
 }
