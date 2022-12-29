e2a2fe88ae29e13231b75664ce35ac57ab9bb00f
Saman <samaneghbali1@gmail.com>
2022-12-29 16:07:48 -0500

    Register Form

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import { useForm } from 'react-hook-form';
-const EMAIL_PATTERN = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+// eslint-disable-next-line
+const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const LoginForm = ({ onSubmit }) => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
