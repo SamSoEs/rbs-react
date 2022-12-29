600b16b78529e7cf3b0226e48ecc8286f168f805
Saman <samaneghbali1@gmail.com>
2022-12-29 15:55:20 -0500

    Login Form Validation

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,44 +1,56 @@
 
 import React from 'react';
 import { useForm } from 'react-hook-form';
+const EMAIL_PATTERN = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+const LoginForm = ({ onSubmit }) => {
 
-const LoginForm = ({onSubmit}) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const { register, handleSubmit } = useForm();
-  
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input
-          ref={register}
-          name="email"
-          type="email"
-          className="form-control"
-          id="email" />
-        {/* <div className="alert alert-danger">
-          <div>
-            Email is required.
-          </div>
-          <div>
-            Must be valid email format!
-          </div>
-        </div> */}
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input 
-          ref={register}
-          name="password"
-          type="password"
-          className="form-control"
-          id="password" />
-      </div>
-      <button 
-        type="submit" 
-        className="btn btn-bwm-main">Submit</button>
-    </form>
-  )
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                    {... register("email",{ required: true, pattern: EMAIL_PATTERN })}
+                    name="email"
+                    type="email"
+                    className="form-control"
+                    id="email" />
+                {errors.email &&
+                    <div className="alert alert-danger">
+                        {errors.email.type === 'required' &&
+                            <span>Email is required!</span>
+                        }
+                        {errors.email.type === 'pattern' &&
+                            <span>Not valid email format!</span>
+                        }
+                    </div>
+                }
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                    {... register("password", { required: true, minLength: 8 })}
+                    name="password"
+                    type="password"
+                    className="form-control"
+                    id="password" />
+                {errors.password &&
+                    <div className="alert alert-danger">
+                        {errors.password.type === 'required' &&
+                            <span>Password is required!</span>
+                        }
+                        {errors.password.type === 'minLength' &&
+                            <span>Minimum length of password is 8 characters!</span>
+                        }
+                    </div>
+                }
+            </div>
+            <button
+                type="submit"
+                className="btn btn-bwm-main">Submit</button>
+        </form>
+    )
 }
 
 export default LoginForm;
