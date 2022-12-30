41868d20fbe84bcc935c82a98da53620eef70d79
Saman <samaneghbali1@gmail.com>
2022-12-30 11:00:14 -0500

    User Model

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+//routes
 const rentalRoutes = require('./routes/rentals');
 const usersRoutes = require('./routes/users');
+
+
+//models
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 mongoose.connect('mongodb+srv://samaneghbali1:Sa12344321@cluster0.kx9khxy.mongodb.net/rbs-react?retryWrites=true&w=majority', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 }, () => {
   console.log('Connected to DB!')
 });
 
+
+//middlewares
 app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
