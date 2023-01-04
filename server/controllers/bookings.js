10d18ccc3ff77dcb7c92de6c53ef33b942e4086e
Saman <samaneghbali1@gmail.com>
2023-01-04 16:04:55 -0500

    Check Booking Validity(Backend)

--- server/controllers/bookings.js
+++ server/controllers/bookings.js
@@ -1,28 +1,72 @@
 
 const Booking = require('../models/booking');
+const moment = require('moment');
 
 
-exports.createBooking = (req, res) => {
-    const bookingData = req.body;
-    const booking = new Booking({...bookingData, user: res.locals.user});
+exports.createBooking = async (req, res) => {
+  const bookingData = req.body;
+  const booking = new Booking({ ...bookingData, user: res.locals.user });
+  if (!checkIfBookingDatesAreValid(booking)) {
+    return res.sendApiError(
+      {
+        title: 'Invalid Booking',
+        detail: 'Dates are invalid!'
+      });
+  }
+  try {
+    const rentalBookings = await Booking.find({ rental: booking.rental });
+    const isBookingValid = checkIfBookingIsValid(booking, rentalBookings);
+    if (isBookingValid) {
+      const savedBooking = await booking.save();
+      return res.json({ startAt: savedBooking.startAt, endAt: savedBooking.endAt })
+    }
+    else {
+      return res.sendApiError(
+          {
+            title: 'Invalid Booking',
+            detail: 'Choosen dates are already taken!'
+          });
+    }
+  }
+  catch (error) {
+    return res.mongoError(error);
+  }
+}
 
-    Booking.find({rental: booking.rental}, (error, rentalBookings) => {
-      if (error) { return res.mongoError(error); }
-  
-      const isValid = checkIfBookingIsValid(booking, rentalBookings);
-      if (isValid) {
-        booking.save((error, savedBooking) => {
-          if (error) { return res.mongoError(error); }
-  
-          return res.json({startAt: savedBooking.startAt, endAt: savedBooking.endAt})
-        })
-      } else {
-        return res.json({message: 'Booking is NOT created!'});
-      }
+// TODO: Provide actual implementation
+function checkIfBookingDatesAreValid(booking) {
+  let isValid = true;
+
+  if (!booking.startAt || !booking.endAt) {
+    isValid = false;
+  }
+
+  if (moment(booking.startAt) > moment(booking.endAt)) {
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+function checkIfBookingIsValid(pendingBooking, rentalBookings) {
+  let isValid = true;
+
+  // rentalBookings.find(booking => booking.startAt > )
+
+  if (rentalBookings && rentalBookings.length > 0) {
+
+    isValid = rentalBookings.every(booking => {
+      const pendingStart = moment(pendingBooking.startAt);
+      const pendingEnd = moment(pendingBooking.endAt);
+
+      const bookingStart = moment(booking.startAt);
+      const bookingEnd = moment(booking.endAt);
+
+      return ((bookingStart < pendingStart) && bookingEnd < pendingStart ||
+        (pendingEnd < bookingEnd && pendingEnd < bookingStart));
+
     })
   }
-  
-  // TODO: Provide actual implementation
-  function checkIfBookingIsValid(booking, rentalBookings) {
-    return true;
-  }
+
+  return isValid
+}
